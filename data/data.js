1f3b0083534a7dda28822beff448cca82e63561e
Abdo-max34 <Abdo-max34>
2024-12-05 14:20:12 +0100

    Auto-update: Thu 12/05/2024 14:20:12.30

--- data/data.js
+++ data/data.js
@@ -1,11 +1,12 @@
 import SteamCard from '@/public/steam-card.gif';
 import Canva from '@/public/CanvaProAccouts.gif';
+import netflixshahid from '@/public/netflixshahidpop.png';
+import spoitune from '@/public/spoitune.png';
 export const Services = [
     // Steam Cards
     {
       id: 2,
       name: 'Steam Cards',
-      price: '$500',
       imageSrc: SteamCard,  // Add your image URL here
       imageAlt: 'Steam Cards : 5$, 10$, 20$, 50$, 100$ ...',
       description:'We provide all Steam Cards.',
@@ -13,25 +14,24 @@ export const Services = [
     },
     {
       id: 3,
-      name: 'SEO Optimization',
-      price: '$30',
+      name: 'Canva Accounts',
       imageSrc: Canva,
-      imageAlt: 'SEO optimization services',
+      imageAlt: 'Canva Accounts (1/3/6/12)months / life time',
+      description:'We provide Canva Accounts .',
       href: '/services/2',
     },
     {
       id: 4,
-      name: 'Digital Marketing',
-      price: '$400',
-      imageSrc: '/images/digital-marketing.jpg',
-      imageAlt: 'Digital marketing services',
+      name: 'Movies & Series',
+      imageSrc: netflixshahid,
+      imageAlt: 'We provide all Netflix & Shahid Accounts (1/3/6/12)months .',
+      description:'We provide all Steam Cards.',
       href: '/services/3',
     },
     {
       id: 5,
-      name: 'Content Creation',
-      price: '$350',
-      imageSrc: '/images/content-creation.jpg',
+      name: 'Spotiy & Apple Music',
+      imageSrc: spoitune,
       imageAlt: 'Content creation services',
       href: '/services/4',
     },

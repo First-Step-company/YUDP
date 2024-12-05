b23ef631068900493e6fb332a4bdf9f69bb63cf4
Abdo-max34 <Abdo-max34>
2024-12-05 20:29:44 +0100

    Auto-update: Thu 12/05/2024 20:29:44.40

--- app/page.js
+++ app/page.js
@@ -18,17 +18,17 @@ export default function ServicesPage() {
           {services.map((service) => (
             <div key={service.id} className="flex group relative flex-col items-center border border-black/15 rounded-3xl">
                 <Image src={service.imageSrc} width={300} height={300} alt={service.imageAlt}/>
-              <div className="mt-4 flex items-center">
-                <div className='flex flex-col mr-6'>
-                  <h3 className="text-sm text-gray-700">
+              <div className="mt-4 flex items-center ">
+                <div className='flex flex-col w-full'>
+                  <h3 className="text-sm text-gray-700 font-semibold text-center w-full">
                     <Link href={service.href}>
                         <span aria-hidden="true" className="absolute inset-0" />
                         {service.name}
                     </Link>
                   </h3>
-                  <p className="mt-1 text-sm text-gray-500">{service.imageAlt}</p>
+                  <p className="mt-1 text-sm text-gray-500 text-center">{service.imageAlt}</p>
                 </div>
-                <p className="rounded-full text-white font-bold bg-red-600 w-10 text-center">{service.price}</p>
+                <p className="rounded-full text-white font-bold bg-red-600 w-1/2 text-center">{service.price}</p>
                 {/* Contact button */}
               {/* <button
                 type="button"

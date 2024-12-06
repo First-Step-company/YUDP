32c29106aa48fa2b5f4c1204f1eca064e9995006
Abdo-max34 <Abdo-max34>
2024-12-06 12:31:44 +0100

    Auto-update: Fri 12/06/2024 12:31:44.36

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function ServicesPage() {
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">Our Services</h2>
 
-          <div className="mt-6 mr-[20%]  grid grid-cols-1 gap-y-4 gap-x-4 sm:grid-cols-2 sm:gap-y-20 sm:gap-x-20 lg:grid-cols-3 lg:gap-y-8 lg:gap-x-8">
+          <div className="mt-6 mr-[40%]  grid grid-cols-1 gap-y-4 gap-x-4 sm:grid-cols-2 sm:gap-x-96 lg:grid-cols-3 lg:gap-y-8 lg:gap-x-8">
         {Services.map((service) => (
             <Link key={service.id} href={`/services/${service.id - 1}`}>
                 <CardDefault service={service} className="h-full w-full p-4 rounded-lg border shadow-sm" />
